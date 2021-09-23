a37402d37ea582c87bbec9ab9fd4e7e461f259c1
theDanielTo <dto1989@gmail.com>
2021-09-23 16:29:21 -0700

    added add.php function that adds todos to db. changed input outline color on focus

--- index.php
+++ index.php
@@ -2,12 +2,24 @@
 
 <div class="main-section">
   <div class="add-section rnd-brdr">
-    <form action="">
-      <input type="text" name="title" class="rnd-brdr" placeholder="Add a todo..." />
-      <button type="submit" class="rnd-brdr">
-        Add
-        <i class="far fa-plus-square"></i>
-      </button>
+    <form action="app/add.php" method="POST" autocomplete="off">
+      <?php if (isset($_GET['mess']) && $_GET['mess'] == 'error') { ?>
+        <input type="text"
+                name="title"
+                class="rnd-brdr"
+                placeholder="This field is required!"
+                style="border-color:#ff6666" />
+        <button type="submit" class="rnd-brdr">
+          Add
+          <i class="far fa-plus-square"></i>
+        </button>
+      <?php } else { ?>
+        <input type="text" name="title" class="rnd-brdr" placeholder="Whatcha doing?" />
+        <button type="submit" class="rnd-brdr">
+          Add
+          <i class="far fa-plus-square"></i>
+        </button>
+      <?php } ?>
     </form>
   </div>
 
@@ -36,7 +48,7 @@
           <h2><?php echo $todo['title']; ?></h2>
         <?php } ?>
         <br>
-        <small>Created: <?php echo $todo['date-time']; ?></small>
+        <small>Created: <?php echo $todo['date_time']; ?></small>
       </div>
     <?php } ?>
   </div>
