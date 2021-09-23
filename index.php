b291b92fe29f4e04ab3c18c377f63af8d5a5ce45
theDanielTo <dto1989@gmail.com>
2021-09-23 13:58:50 -0700

    more css & put 'section' css into layout.css. added fontawesome. todos rendered from db.

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <form action="">
       <input type="text" name="title" class="rnd-brdr" placeholder="Add a todo..." />
       <button type="submit" class="rnd-brdr">
-        Add &nbsp;
-        <span>&#43;</span>
+        Add
+        <i class="far fa-plus-square"></i>
       </button>
     </form>
   </div>
@@ -16,7 +16,7 @@
   ?>
 
   <div class="show-todo-section rnd-brdr">
-    <?php if ($todos->rowCount() > 0) { ?>
+    <?php if ($todos->rowCount() <= 0) { ?>
       <div class="todo-item rnd-brdr">
         <div class="empty rnd-brdr">
           <img src="img/todo-img.webp" alt="todo">
@@ -25,13 +25,14 @@
       </div>
     <?php } ?>
 
-
-    <div class="todo-item rnd-brdr">
-      <input type="checkbox">
-      <h2>This is a todo item</h2>
-      <br>
-      <small>created: 09/23/2021</small>
-    </div>
+    <?php while ($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
+      <div class="todo-item rnd-brdr">
+        <input type="checkbox">
+        <h2>This is a todo item</h2>
+        <br>
+        <small>Created: 09/23/2021</small>
+      </div>
+    <?php } ?>
   </div>
 </div>
 
